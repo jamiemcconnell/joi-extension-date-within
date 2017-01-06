95098b3611e7110a1fb51c351f1d9192836f1452
Jamie McConnell <jamie_mcconnell@icloud.com>
2017-01-06 15:50:29 +0000

    refactor and change base type (#5)
    * refactor and change base type
    
    * coverage for description

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 'use strict';
 
-var joi = require('joi');
+var Joi = require('joi');
 var hoek = require('hoek');
 const dayms = 86400000;
 
-const withinDaysRule = {
-  name: 'withinDays',
-  params: {
-    days: joi.number().min(0),
-    from: joi.any()
-  },
-  validate: function(params, value, state, options) {
-    var from = params.from;
-
-    if(typeof from === 'undefined') {
-      from = new Date();
-    }
-
-    // If 'from' is a ref get the actual value.
-    if(joi.isRef(params.from)) {
-      from = hoek.reach(state.parent, params.from.key);
-
-      if(typeof from === 'undefined') {
-        return this.createError('date.withinDaysRefError', {}, state, options);
-      }
-    }
-
-    return (Math.ceil((value - from) /dayms) > params.days) ? this.createError('date.withinDays', { v: value, days: params.days, from: from }, state, options) : true;
-  }
-};
-
-const rules = [].concat([withinDaysRule]);
-
-const extension = {
-  base: joi.date(),
+module.exports = {
   name: 'date',
+  base: Joi.any(),
   language: {
     withinDays: 'must be within {{days}} days of {{from}}',
     withinDaysRefError: 'invalid Joi.ref in schema'
   },
-  rules: rules
-};
+  coerce(value, state, options) {
+    if(typeof this._from === 'undefined') {
+      this._from = new Date();
+    }
 
-module.exports = extension;
+    // If 'from' is a ref get the actual value.
+    if(Joi.isRef(this._from)) {
+      this._from = hoek.reach(state.parent, this._from.key);
+
+      if(typeof this._from === 'undefined') {
+        return this.createError('date.withinDaysRefError', {}, state, options);
+      }
+    }
+
+    return (Math.ceil((value - this._from) /dayms) > this._days) ? this.createError('date.withinDays', { v: value, days: this._days, from: this._from }, state, options) : true;
+
+  },
+  rules: [
+    {
+      name: 'withinDays',
+      description(params) {
+        return `Date should with within ${params.days} of ${params.from}`;
+      },
+      params: {
+        days: Joi.number().min(0).required(),
+        from: Joi.any().default(undefined) // allows for a ref or date - being lazy!
+      },
+      setup(params) {
+        this._days = params.days;
+        this._from = params.from;
+      },
+      validate(params, value, state, options) {
+        // No-op just to enable description
+        return value;
+      }
+    }
+  ]
+};
